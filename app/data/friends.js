9be7cebd7b8780a2dfa4c52cdef7af3b98016b49
Csgraser <csgraser@gmail.com>
2018-04-26 18:36:18 -0600

    trying my best...

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// ===============================================================================
+var friends = [
+		{
+			"name": "Sephiroth",
+			"photo": "/assets/images/sephiroth.jpg",
+			"scores": [
+				// 21
+				5,
+				3,
+				4,
+				4,
+				1,
+				4
+			]
+		},
+		{
+			"name": "The Joker",
+			"photo": "/assets/images/joker.jpg",
+			"scores": [
+				// 22
+				2,
+				5,
+				1,
+				4,
+				5,
+				5
+			]
+		},
+		{
+			"name": "Carnage",
+			"photo": "/assets/images/carnage.jpeg",
+			"scores": [
+				// 13
+				1,
+				1,
+				2,
+				4,
+				3,
+				2
+			]
+		},
+		{
+			"name": "Loki",
+			"photo": "/assets/images/Loki.jpg",
+			"scores": [
+				// 19
+				2,
+				1,
+				5,
+				3,
+				3,
+				5
+			]
+		},
+		{
+			"name": "Handsome Jack",
+			"photo": "/assets/images/Handsome_jack.jpg",
+			"scores": [
+				// 18
+				1,
+				1,
+				2,
+				5,
+				5,
+				4
+			]
+		},
+		{
+			"name": "Ganondorf",
+			"photo": "/assets/images/ganon.jpg",
+			"scores": [
+				// 17
+				1,
+				5,
+				5,
+				1,
+				1,
+				4
+			]
+		},
+		{
+			"name": "Diablo",
+			"photo": "/assets/images/diablo.jpg",
+			"scores": [
+				// 11
+				1,
+				1,
+				5,
+				1,
+				1,
+				2
+			]
+		},
+	]
+	
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
